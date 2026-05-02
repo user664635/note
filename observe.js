b5840c2463431a741540f97ed92cef01d2ae397a
user664635 <143943316+user664635@users.noreply.github.com>
2026-05-02 14:52:02 +0800

    Add monitoring functionality to observe.js

--- observe.js
+++ observe.js
@@ -20,3 +20,23 @@ function defineReactive(obj, key, val) {
     }
   });
 }
+
+const monitor = new Proxy(obj, {
+  set(target, property, value, receiver) {
+    val = target[property];
+    (delta = value - val) && console.log(`${property} : ${val} (${delta})`);
+    return Reflect.set(target, property, value, receiver);
+  },
+});
+
+let ss0 = obj;
+setInterval(() => {
+  const ss1 = obj;
+  Object.keys(ss1).forEach(key => {
+    v0 = ss0[key];
+    v1 = ss1[key];
+    typeof v0 == 'number' && typeof v1 == 'number' && v1 - v0 &&
+    console.log(`${key} : ${v1} (${v1 - v0})`);
+  });
+  ss0 = ss1;
+}, 100);
